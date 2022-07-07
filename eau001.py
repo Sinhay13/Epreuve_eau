bc14fd0539d1547583ba7bec25818c14c7fdfcec
Yahnis <yahnis.allegret@yahoo.com>
2022-07-07 08:59:23 +0200

    Finalisatio de eau 001

--- eau001.py
+++ eau001.py
@@ -1,3 +1,21 @@
+#fonction pour ajouter des 0 sur une variable str
+def ajout_zero (x):
+    if len(x) <2:
+        x=f"0{x}"
+    return x
+
+
+# fonction pour transformé chaque nombre de la list en int 
+def int_str(nb):
+    li=nb.split(" ")
+    A= int(li[0])
+    B= int(li[1])
+    return A ,B
+
+
+
+
+#fonction pour generer deux listes de deux chiffre de 0 à 99 en str
 def generator_lists():
     l1=[]
     l2=[]
@@ -10,12 +28,11 @@ def generator_lists():
         l1_str.append(str(l1[i]))
         l2_str.append(str(l2[i]))
     for i in range(len(l1_str)):
-        if len(l1_str[i])<2:
-            l1_str[i]=f"0{l1_str[i]}"
-            l2_str[i]=f"0{l2_str[i]}"
+        l1_str[i]=ajout_zero(l1_str[i])
+        l2_str[i]=ajout_zero(l2_str[i])
     return l1_str, l2_str
 
-
+#fonction pour faire des deux une liste une seul liste en prenant en compte la totalité des combinaisons avec repetition 
 def combinaisons(l1,l2):
     l=[]
     n=0
@@ -26,12 +43,34 @@ def combinaisons(l1,l2):
             l.append(nb)
     return l
 
+# fonction pour trier chaque element de la la liste en ordre croissant
+def tri(l):
+    n=0
+    for i in l:
+        A,B=int_str(l[n])
+        A_str= str(A)
+        B_str=str(B)
+        A_str=ajout_zero(A_str)
+        B_str=ajout_zero(B_str)
+        if B < A:
+            l[n]= f"{B_str} {A_str}"
+        n+=1
+    return l
+
+# fonction pour créer une nouvelle liste sans les variables identique 
+def same(l):
+    l_final=[]
+    for i in range(len(l)):
+        if l[i] not in l_final:
+            l_final.append(l[i])
+    return l_final
+
+
+# fonction pour suprimer les valeurs quand les deux nombre sont identique
 def double(l):
     n=0
     for i in l:
-        li=l[n].split(" ")
-        A= int(li[0])
-        B= int(li[1])
+        A,B=int_str(l[n])
         if A==B:
             del l[n]
         n+=1
@@ -40,12 +79,15 @@ def double(l):
 
 
 
+# Appel des fonctions : 
+
 l1, l2 = generator_lists()
 l= combinaisons(l1,l2)
+l=tri(l)
 l=double(l)
-print(l)
+l_final= same(l)
+print(l_final)
 
 
 
 
-# trouver comment suprimer les nombres en doublette ordre croissant
