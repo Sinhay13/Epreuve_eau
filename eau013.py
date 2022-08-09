1972efcd299861aa45df7def9e17682ca6d257ed
Yahnis <yahnis.allegret@yahoo.com>
2022-08-09 20:40:01 +0200

    final 13

--- eau013.py
+++ eau013.py
@@ -21,3 +21,45 @@ Afficher error et quitter le programme en cas de problèmes d’arguments.
 
 Wikipedia vous présentera une belle description de cet algorithme de tri.
 '''
+
+import sys
+
+
+#Recuperation DATA :
+def input_data():
+    l = sys.argv[1:]
+    return l
+
+
+# Test d'erreur:
+def error(l):
+    if len(l) <= 1:
+        print("error")
+        sys.exit()
+    l_tested = []
+    for i in range(len(l)):
+        try:
+            l_tested.append(int(l[i]))
+        except:
+            print("error")
+            sys.exit()
+    return l_tested
+
+
+# Tri par selaction:
+def tri_selection(l):
+    for i in range(len(l)):
+        min = i
+        for j in range(i + 1, len(l)):
+            if l[min] > l[j]:
+                min = j
+        tmp = l[i]
+        l[i] = l[min]
+        l[min] = tmp
+    print(l)
+
+
+# Appels des fonctions :
+l = input_data()
+l = error(l)
+tri_selection(l)
