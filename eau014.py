c5ceb103ea60b3824d457feb839cf6112d60dab7
Yahnis <yahnis.allegret@yahoo.com>
2022-10-15 21:46:37 +0200

    travaille

--- eau014.py
+++ eau014.py
@@ -14,3 +14,40 @@ Afficher error et quitter le programme en cas de problèmes d’arguments.
 
 '''
 
+import sys
+
+#Recuperation DATA +teste d'error
+def input_data (): 
+  l_int=0  
+  l = sys.argv[1:]
+  if len(l)<2:
+      print("error")
+      sys.exit()
+  else:
+      return l
+
+
+def comparaison (l):
+    dico=dict()
+    N=len(l)
+    n=0
+    while n<N:
+        
+
+        
+
+ 
+
+
+
+l= input_data()
+l2=sorted(l,reverse=False)
+print(l2)
+
+
+
+
+
+
+
+
