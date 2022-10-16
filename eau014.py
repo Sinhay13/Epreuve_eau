671c9bd44f8771dc6a7235586d12716e7cd7a340
Yahnis <yahnis.allegret@yahoo.com>
2022-10-16 09:05:32 +0200

    travaille sur 14

--- eau014.py
+++ eau014.py
@@ -26,23 +26,43 @@ def input_data ():
   else:
       return l
 
+# creation d'une liste avec la valeur ascii des lettre ce trouven a n position: 
+def liste_ascii (l, n=0):
+    l_ascii=[]
+    for word in l:
+        l_ascii.append(str(ord(word[n])))
+    return l_ascii
+    
 
-def comparaison (l):
-    dico=dict()
-    N=len(l)
-    n=0
+# creation d'un dictionaire avec les deux listes:
+def dictionaire(l, l_ascii):
+    dico= dict(zip(l_ascii, l))
+    cle= sorted(dico)
+    return dico, cle
+
+#teste des lettre en double:
+def doublette (l, dico, cle, n=0):
+    N=len(cle)
     while n<N:
-        
-
-        
-
- 
+        pass
 
 
+# creation d'une liste ordoner et affichage du resultat:
+def trier(dico, cle):
+    l_final=[]
+    for i in range(len(cle)):
+        l_final.append(dico[cle[i]])
+    phrase=" ".join(l_final)
+    print(phrase)
 
+      
+#appel des fonction:
 l= input_data()
-l2=sorted(l,reverse=False)
-print(l2)
+l_ascii=liste_ascii(l)
+dico,cle= dictionaire(l, l_ascii)
+#dico,cle=doublette(dico, cle)
+trier(dico, cle)
+
 
 
 
